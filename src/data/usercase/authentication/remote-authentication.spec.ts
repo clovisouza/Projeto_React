c7e4f98943d246b97d984826c26be330e813898b
unknown <clovis.souza2011@hotmail.com>
2021-01-12 02:23:02 -0300

    Subindo novos testes

--- src/data/usercase/authentication/remote-authentication.spec.ts
+++ src/data/usercase/authentication/remote-authentication.spec.ts
@@ -48,6 +48,30 @@ describe('RemoteAuthentication', () => {
         }   
         const promise = sut.auth(mockAuthentication())
        await expect(promise).rejects.toThrow(new InvalidCredentialsError())
+    
+
+    });
+
+    test('Should throw unexpected error if HttpPostClient returns 500', async () => {
+        
+        const { sut, httpPostClientSpy } = makeSut()     
+        httpPostClientSpy.response = {
+            statusCode:HttpStatusCode.serverError
+        }   
+        const promise = sut.auth(mockAuthentication())
+       await expect(promise).rejects.toThrow(new InvalidCredentialsError())
+    
+
+    });
+    test('Should throw unexpected error if HttpPostClient returns 404', async () => {
+        
+        const { sut, httpPostClientSpy } = makeSut()     
+        httpPostClientSpy.response = {
+            statusCode:HttpStatusCode.notFound
+        }   
+        const promise = sut.auth(mockAuthentication())
+       await expect(promise).rejects.toThrow(new InvalidCredentialsError())
+    
 
     });
 
