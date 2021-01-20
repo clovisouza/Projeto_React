17d63c1e662ddb6bbaf05f44004027d2cf157bc8
unknown <clovis.souza2011@hotmail.com>
2021-01-20 16:36:09 -0300

    refactor de teste

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,18 +1,32 @@
 import { AxiosHttpClient } from './axios-http-client'
 import axios from 'axios'
-import faker from 'faker'
+import faker, { fake } from 'faker'
+import { HttpPostParams } from '@/data/protocols/http'
 
 jest.mock('axios')
 
 const mockedAxios = axios as jest.Mocked<typeof axios>
+const makeSut = (): AxiosHttpClient=>{
+    return new AxiosHttpClient()
+}
 
+const mockPostRequest=():HttpPostParams<any>=>({
+    url:faker.internet.url(),
+    body:faker.random.objectElement()
+})
 
 describe('AxiosHttpClient', () => {
-    test('Should Call axios with correct URL', async () => {
-        const url = faker.internet.url()
-        const sut = new AxiosHttpClient()
-        sut.post({url})
-        expect(mockedAxios).toHaveBeenCalledWith(url)
+    test('Should Call axios with correct values', async () => {
+        const request = mockPostRequest()
+        const sut = makeSut()
+        await sut.post(request)
+        expect(mockedAxios.post).toHaveBeenCalledWith(request.url,request.body)
 
     })
+
+
+ 
 })
+
+
+
