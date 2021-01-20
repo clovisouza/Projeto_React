a70d51cc91fe03e7a29e8c141e63d04bba194eea
unknown <clovis.souza2011@hotmail.com>
2021-01-20 13:00:18 -0300

    ajuste no teste

--- src/data/usercase/authentication/remote-authentication.spec.ts
+++ src/data/usercase/authentication/remote-authentication.spec.ts
@@ -1,13 +1,12 @@
 
-import { HttpPostClientSpy } from "@/data/test/mock-http-client";
-import faker from 'faker'
-import { mockAccountModel, mockAuthentication } from "@/domain/test/mock-account";
+import { HttpPostClientSpy } from "@/data/test";
+import faker from 'faker';
+import { mockAccountModel, mockAuthentication } from "@/domain/test";
 import { RemoteAuthentication } from "./remote-authentication";
-import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-error";
-import { HttpStatusCode } from "@/data/protocols/http/http-response";
-import { UnexpectedError } from "@/domain/errors/unexpected-error";
-import { AuthenticationParams } from "@/domain/usecases/authentication";
-import { AccountModel } from "@/models/account-model";
+import { InvalidCredentialsError,UnexpectedError  } from "@/domain/errors";
+import { HttpStatusCode } from "@/data/protocols/http";
+import { AuthenticationParams } from "@/domain/usecases";
+import { AccountModel } from "@/models";
 
 
 type SutTypes = {
--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,15 +1,15 @@
 
-import { AuthenticationParams } from "@/domain/usecases/authentication";
-import { AccountModel } from "@/models/account-model";
+import { AuthenticationParams } from "@/domain/usecases";
+import { AccountModel } from "@/models";
 import faker from "faker";
 
 export const mockAuthentication = ():AuthenticationParams=>({
 email:faker.internet.email(),
 password:faker.internet.password()
 
-})
+});
 
 export const mockAccountModel = ():AccountModel=>({
     accessToken: faker.random.uuid()
     
-    })
+});
